97f70879d24f0fc9293952880a03c0f82c30f3f3
Fletcher Wilson <wilsonf@southwestern.edu>
2026-07-24 16:45:32 -0500

    Prelim Test Helpers

---
+++ test/helpers.py
@@ -0,0 +1,48 @@
+"""
+Simple helper functions that instatiate randomized test vectors and calculates their dot prodcuts 
+to test the chip against. 
+
+If you couldn't tell by their names, dotProd() calculates and returns the dot product of two vectors (list[int])
+and randomVecs() initializes two randomized vectors whose values fall within the range of the signed int8s that the
+chip is designed to process.
+"""
+
+import random 
+
+
+def dotProd(vec1: list[int], vec2: list[int]) -> int:
+    """
+    Calculates and returns the dot product of two vectors
+
+    args:
+        vec1: list[int] | first vector, maybe the weight vector for a single neuron in the nth layer
+        vec2: list[int] | second vector, maybe the activation vector for the neuron layer n - 1  
+    
+    returns:
+        their dot product lol
+    """
+    assert len(vec1) == len(vec2) 
+
+    return sum(a * b for a, b in zip(vec1, vec2))
+    
+
+
+
+def randomVecs():
+    """ 
+    Initializes and returns two vectors (list[int]) with random integer values within 
+    the valid signed int8 range
+
+    returns:
+        v1, v2: list[int] | two vectors initialized with 5-200 dimensions 
+    """
+    listlen = random.randint(5, 200)
+
+    v1 = []
+    v2 = []
+    for _ in range(listlen):
+        v1.append(random.randint(-128, 127))
+        v2.append(random.randint(-128, 127))
+
+    return v1, v2
+        
